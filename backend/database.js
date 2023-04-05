dd6b7d3c34e3895b4494bf5c117e030f9dba7929
Mithila06 <128927175+Mithila06@users.noreply.github.com>
2023-04-05 08:32:13 +0530

    Add files via upload

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,5 @@
 const dbConnection = require("./sqlite");
+const testBase = require("../backend/test/testBase");
 
 dbConnection
   .getDbConnection()
@@ -55,8 +56,8 @@ const addTeacher = async (id, name, age) => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id, name, age])
-            .then(() => {
-                resolve({status: "Successfully inserted Teacher"})
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -65,12 +66,12 @@ const addTeacher = async (id, name, age) => {
 }
 
 const updateTeacher = async (name, age, id) => {
-    const sql = `UPDATE teacher SET name=?, age=? WHERE id=?`
+    const sql = `UPDATE teacher SET name=?,age=? WHERE id = ? `
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [name, age, id])
-            .then(() => {
-                resolve({status: "Successfully updated Teacher"})
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -83,8 +84,8 @@ const deleteTeacher = async (id) => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id])
-            .then(() => {
-                resolve({status: "Successfully deleted Teacher"})
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -97,8 +98,8 @@ const readStudents = async () => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
-            .then((student) => {
-                resolve(student);
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -111,8 +112,8 @@ const readStudentInfo = async (id) => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id])
-            .then((student) => {
-                resolve(student);
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -125,8 +126,8 @@ const addStudent = async (id, name, age, hometown) => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id, name, age, hometown])
-            .then(() => {
-                resolve({status: "Successfully inserted Student"})
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -134,13 +135,13 @@ const addStudent = async (id, name, age, hometown) => {
     });
 }
 
-const updateStudent = async (name, age, id, hometown) => {
-    const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
+const updateStudent = async (name, age, hometown, id) => {
+    const sql = `UPDATE student SET name=?,age=?,hometown=? WHERE id = ?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [name, age, id, hometown])
-            .then(() => {
-                resolve({status: "Successfully updated Student"})
+            .raw(sql, [name, age, hometown, id])
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -153,15 +154,14 @@ const deleteStudent = async (id) => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id])
-            .then(() => {
-                resolve({status: "Successfully deleted Student"})
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
             });
     });
 }
-
 
 module.exports = {
     readTeachers,
@@ -173,5 +173,6 @@ module.exports = {
     readStudentInfo,
     readTeacherInfo,
     updateStudent,
-    updateTeacher
+    updateTeacher,
+    dbinitialize
 };
